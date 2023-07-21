341c3bb3630088d533d3cc663a6c7e276539e1ba
Vishnupriya Karanam <kvpriya729@gmail.com>
2023-07-21 08:56:18 +0530

    Add files via upload

---
+++ python/MyPackage/Addition.py
@@ -0,0 +1,8 @@
+def add(*a):
+    sum = 0
+    for x in a:
+        sum += x
+    return sum
+
+def sum(a,b):
+    return a+b
---
+++ python/MyPackage/Multiplication.py
@@ -0,0 +1,8 @@
+def multiply(*a):
+    result = 1
+    for x in a:
+        result *= x
+    return result
+
+def exponent(a,b):
+    return a**b
